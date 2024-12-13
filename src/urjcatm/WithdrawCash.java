a2a87b8aa86dc8dc6b4d903ca5f7b5c349395658
34661 <34661@LAPTOP-UIMDMPIC>
2024-12-13 13:08:53 +0100

    GAY QUIEN LO LEA

--- src/urjcatm/WithdrawCash.java
+++ src/urjcatm/WithdrawCash.java
@@ -3,41 +3,83 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package urjcatm;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.CommunicationException;
 import sienens.ATM;
+import urjc.*;
 
 /**
  *
  * @author dadig
  */
+
 public class WithdrawCash extends TitledOperation{
     //Constructor
     public WithdrawCash(OperationContext op){
         super(op);
     }
+  
     @Override
     public boolean doOperation(){
         ATM atm = super.getOperationContext().getAtm();
+        OperationContext context = super.getOperationContext();
+        UrjcBankServer server = context.getServer();
         
         for(int i=0; i < 6; i++)
             atm.setOption(i, null);
         
-        atm.setTitle("Ingrese la cantidad a retirar");
+        try {
+        long accountId = atm.getCardNumber();
+        int saldoDisponible = server.avaiable(accountId);
+        atm.setTitle("Saldo disponible: " + saldoDisponible + "€\nIngrese la cantidad a retirar\n");
+        } catch (CommunicationException ex) {
+            Logger.getLogger(WithdrawCash.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
         char event = atm.waitEvent(30);
         String cadena = "";
+        
         while (event >= '0' && event <= '9') {
             cadena += event;
             atm.setInputAreaText(cadena + " €");
             event = atm.waitEvent(30);
         }
-        if (event % 10 != 0){
-            atm.setInputAreaText("Cantidad no disponible");
+    
+        int dinero;
+        try {
+            dinero = Integer.parseInt(cadena);
+        } catch (NumberFormatException e) {
+            atm.setInputAreaText("Error al ingresar la cantidad.");
+            return false;
         }
+    
+        if (dinero % 10 != 0){
+            atm.setInputAreaText("Cantidad no disponible");
+            return false;
+        }
+    
+        try {
+            long accountId = atm.getCardNumber();
+            if(dinero <= server.avaiable(accountId)){
+                atm.setInputAreaText("Retiro exitoso. Retire su dinero.");
+            } else {
+                atm.setInputAreaText("Fondos insuficientes.");
+                return false;
+            }
+        } catch (CommunicationException ex) {
+            Logger.getLogger(WithdrawCash.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
         return true;
         
     }
-    @Override
-    public String getTitle(){
-        return "String0";
-    }
 
+    @Override
+    public String getTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
+    
