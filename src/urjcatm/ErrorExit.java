217a77d797a2d72f06a7ca16145386b345f27f7e
Dadigoes <dadigoes03@gmail.com>
2024-12-20 11:24:40 +0100

    Ultimo commit

--- src/urjcatm/ErrorExit.java
+++ src/urjcatm/ErrorExit.java
@@ -28,25 +28,19 @@ public class ErrorExit extends AtmOperation{
         switch (idioma) {
             case ("ES"):
                 atm.setTitle("Error de conexion");
-                //atm.setInputAreaText("Por favor, recoja su tarjeta");
                 break;
             case ("EN"):
                 atm.setTitle("Connection Error");
-                //atm.setInputAreaText("Please, pick up your card");
 
                 break;
             case ("CA"):
                 atm.setTitle("Error de connexió");
-                //atm.setInputAreaText("Per favor, reculli la seva targeta");
                 break;
             case ("EU"):
                 atm.setTitle("Konexio-errorea");
-                //atm.setInputAreaText("Mesedez, jaso zure txartela");
                 break;
             default:
                 atm.setTitle("Error de conexion");
-                //atm.setInputAreaText("Por favor, recoja su tarjeta");
-
         }
     }
 }
